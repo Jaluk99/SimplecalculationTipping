aa99d48bb175f9d0972e3cc3870c0ebf7a177829
Johnny Lukas <johnnylukas99@gmail.com>
2017-11-05 12:40:43 +0100

    Initial commit

---
+++ app/src/main/java/pl/jlukas/simplecalculationtipping/MainActivity.java
@@ -0,0 +1,98 @@
+package pl.jlukas.simplecalculationtipping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText accountEditText;
+    private TextView accountTextView;
+
+    private TextView precentTextView;
+    private SeekBar precentSeekBar;
+
+    private TextView tipTextView;
+
+    private TextView amountTextView;
+
+    private NumberFormat currencyInstance = NumberFormat.getCurrencyInstance();
+    private NumberFormat precentInstance = NumberFormat.getPercentInstance();
+
+    private double billAmount = 0.0;
+
+    private double tipPrecent = 0.15;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        accountEditText = findViewById(R.id.accountEditText);
+        accountTextView = findViewById(R.id.accountTextView);
+
+        precentTextView = findViewById(R.id.precentTextView);
+        precentSeekBar = findViewById(R.id.precentSeekBar);
+
+        tipTextView = findViewById(R.id.tipTextView);
+        amountTextView = findViewById(R.id.amountTextView);
+
+        accountEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    billAmount = Double.parseDouble(s.toString()) / 100.0;
+                    accountTextView.setText(currencyInstance.format(billAmount));
+                } catch(NumberFormatException ex) {
+                    accountTextView.setText("");
+                    billAmount = 0.0;
+                }
+
+                tipAndTotalAmount();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        precentSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tipPrecent = progress / 100.0;
+                tipAndTotalAmount();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void tipAndTotalAmount() {
+        double tipAmount = billAmount * tipPrecent;
+        double totalAmount = tipAmount + billAmount;
+
+        precentTextView.setText(precentInstance.format(tipPrecent));
+        tipTextView.setText(currencyInstance.format(tipAmount));
+        amountTextView.setText(currencyInstance.format(totalAmount));
+    }
+}
